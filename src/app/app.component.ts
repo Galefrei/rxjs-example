ed608983542a71a5700fc0d465268897a1f3f306
Спиридонов Сергей Сергеевич <sspiridonov@sberbank.ru>
2021-08-29 17:52:48 +0300

    Законченный пример со списком задач

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {from, merge, Observable, Subject} from 'rxjs';
+import {map, scan} from 'rxjs/operators';
+
+type TaskUpdate = (tasks: string[]) => string[];
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-example';
+  taskText: string;
+
+  addTask$: Subject<string> = new Subject<string>();
+  removeTask$: Subject<string> = new Subject<string>();
+  taskList$: Observable<string[]>;
+
+  constructor() {
+    const initialTasks$: Observable<TaskUpdate> = from([['Написать пример работы с RxJs в стиле реактивного программирования']]).pipe(
+      map((initialTasks: string[]) => () => initialTasks)
+    );
+
+    const addTaskObs$: Observable<TaskUpdate> = this.addTask$.pipe(
+      map((newTask: string) => (oldTasks: string[]) => [...oldTasks, newTask])
+    );
+
+    const removeTaskObs$: Observable<TaskUpdate> = this.removeTask$.pipe(
+      map((removedTask: string) => (oldTasks: string[]) => oldTasks.filter(t => t !== removedTask))
+    );
+
+    this.taskList$ = merge(initialTasks$, addTaskObs$, removeTaskObs$).pipe(
+      scan((tasks, taskUpdate) => taskUpdate(tasks), [])
+    );
+  }
+
+  addTask(): void {
+    this.addTask$.next(this.taskText);
+  }
+
+  removeTask(task: string): void {
+    this.removeTask$.next(task);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
